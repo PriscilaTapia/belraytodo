dc9c69c02b6f5ba6ba32d9fcda92ed109ec759dd
PriscilaTapia <46427698+PriscilaTapia@users.noreply.github.com>
2021-10-22 17:50:00 -0300

    testing boton editar y eliminar

--- src/components/Todos.test.js
+++ src/components/Todos.test.js
@@ -1,3 +1,18 @@
-// import { fireEvent, render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
-test.skip("button icono editar", () => {});
+const Button = ({ onClick, children }) => (
+  <button onClick={onClick}>{children}</button>
+);
+
+test("boton editar", () => {
+  const handleClick = jest.fn();
+  render(<Button onClick={handleClick}>Editar</Button>);
+  fireEvent.click(screen.getByText(/Editar/i));
+  //   expect(handleClick).toHaveBeenCalledTimes(1)
+});
+test("boton eliminar", () => {
+  const handleClick = jest.fn();
+  render(<Button onClick={handleClick}>Eliminar</Button>);
+  fireEvent.click(screen.getByText(/Eliminar/i));
+  //   expect(handleClick).toHaveBeenCalledTimes(1)
+});
